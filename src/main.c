7e9b7adb2c8eb57490b66d6d8f42a14219b351a9
1sun12 <regan_sun12@outlook.com>
2026-02-17 23:05:33 -0500

    [edu/wait] An example using wait to figure out the status of a child process from fork

--- src/main.c
+++ src/main.c
@@ -1,26 +1,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h> /* wait() */
+#include <stdint.h>  /* pid_t */
+#include <unistd.h> /* fork + sleep */
 
-/**
- * test_ptrace()
- * @brief playing with ptrace, see `man 2 ptrace`
- */
-void test_ptrace() {
-  // code
-}
+int main() {
+  printf("\n\n=== Starting wait example ===\n\n");
+  /* create a pid_t that stores the pid of the child process */
+  pid_t child_pid;
 
-int main () {
-  printf("Hello world!\n");
+  printf("Calling fork()\n");
+  child_pid = fork();
 
-  int *ptr = NULL;
+  /* if pid is -1, fork failed */
+  if (child_pid == -1) {
+    perror("fork");
+    return EXIT_FAILURE;
+  }
 
-  int number = 5;
+  /* if pid is 0, then this is the child process */
+  if (child_pid == 0) {
+    
+    /* child is doing some work... */
+    printf("Child is doing some work...\n");
+    sleep(2);
+    return 42;
+  }
 
-  ptr = &number;
+  /* as the parent, wait for the child to finish working and return something... */
+  int status;
+  waitpid(child_pid, &status, 0);
 
-  printf("Number: %d\n", number);
-  printf("Address: 0x%x\n", ptr);
+  /* defined in <sys/wait.h> are specific macros used in waiting, see `man 2 wait` for full info */
+  if (WIFEXITED(status)){
+    printf("Child exited with the status code: %d\n", WEXITSTATUS(status));
+  } else if (WIFSTOPPED(status)) {
+    printf("Child stopped by signal: %d\n", WSTOPSIG(status));
+  }
 
   return EXIT_SUCCESS;
 }
