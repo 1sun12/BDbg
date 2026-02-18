2e0a2f36f94fced533ca87c1fde4f657ab12fb09
1sun12 <regan_sun12@outlook.com>
2026-02-17 20:20:54 -0500

    [main] remembering some addressable stuff

--- src/main.c
+++ src/main.c
@@ -2,8 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * test_ptrace()
+ * @brief playing with ptrace, see `man 2 ptrace`
+ */
+void test_ptrace() {
+  // code
+}
+
 int main () {
   printf("Hello world!\n");
+
+  int *ptr = NULL;
+
+  int number = 5;
+
+  ptr = &number;
+
+  printf("Number: %d\n", number);
+  printf("Address: 0x%x\n", ptr);
 
   return EXIT_SUCCESS;
 }
