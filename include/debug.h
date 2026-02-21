6c364d92bd703e2bfe096c152cd06cff8dcddae9
1sun12 <regan_sun12@outlook.com>
2026-02-20 22:57:31 -0500

    [wskele] wrote a lot of documentation

--- include/debug.h
+++ include/debug.h
@@ -1,12 +1,13 @@
 
 /**
- * DEBUG_ENABLED 
+ * @title DEBUG_ENABLED 
  * @brief If set to 1, debug messages will print. If set to 0, debug messages will not print
+ * @Use `#define DEBUG_ENABLED 1` (or) `#define DEBUG_ENABLED 0`
  */
 #define DEBUG_ENABLED 1
 
 /**
- *  DBG(msg)
+ *  @title DBG(msg)
  *  @brief Use to print debug messages with some embeded formatting
  *  @Use `DBG("main: Starting main...");`
  */
--- src/main.c
+++ src/main.c
@@ -10,16 +10,56 @@
 
 int main() {
   DBG("main: Starting main...");
-
+  
+  /**
+   *    @title child_pid
+   *    @brief Variable storing the process ID number for the child
+   */
   pid_t child_pid = -1;
+
+  /**
+   *    @title status 
+   *    @brief Variable storing the result of `waitpid()`. `waitpid()` is how the parent is notified...
+   *    of when anything of signifigance happens to the child process.
+   */
   int status = 0;
 
+  /**
+   *    @title *target 
+   *    @brief (execve) Variable storing the string representation of the target program to be      ...
+   *    debugged.
+   */
   char *target = "trgt-prog/trgt-prog";
+
+  /**
+   *    @title *argv[]
+   *    @brief (execve) Variable storing the arguments being passed to the target program. When I   ...
+   *    say arguments, I mean something like `-la` when using `ls -la` in bash.
+   */
   char *argv[] = { NULL };
+
+  /**
+   *    @title *envp[]
+   *    @brief (execve) Variable storing the environment pointers being passed to the target program.
+   */
   char *envp[] = { NULL };
 
+  /**
+   *    if      child_pid is equal to -1 after forking a new child.
+   *    then    print an error message.
+   *    else    just continue.
+   */
+  DBG("main: Creating fork()...");
   if ((child_pid = fork()) == -1) perror("fork");
 
+  /**
+   *    if      child_pid is equal to 0.
+   *        then    THIS is the child process created from the earlier fork. Call `ptrace()` using  ...
+   *                `PTRACE_TRACEME` which gives the parent process tracing capabilities over the   ... 
+   *                child.
+   *    else    child_pid is not the child.
+   *        then    THIS is the parent, use `waitpid()` and other to manipulate the child program.
+   */
   if (child_pid == 0) {
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
     execve(target, argv, envp);
